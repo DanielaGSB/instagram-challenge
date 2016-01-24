5fb55033988247e2271c40c40b0039403769cce6
Daniela <daniela.galeote@gmail.com>
2016-01-24 11:22:00 +0000

    Paperclip installed. User can upload image

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,19 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photos.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(post_params)
+    redirect_to photos_path
+  end
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+
 end
